bd23556c6c6dc08a79712d4adcae0fcde2459566
Ade Indriawan <aindriawan1@gmail.com>
2021-12-08 15:18:19 +0700

    Modify user columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 50);
+            $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 100);
             $table->rememberToken();
-            $table->string('phone');
+            $table->string('phone', 20);
             $table->enum('type', ['Managerial', 'General']);
             $table->integer('unit_id');
             $table->enum('role', ['Lecturer', 'Student', 'Administration', 'Employee']);
