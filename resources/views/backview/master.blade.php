9e6d9562d0b60dce4c7e0be3e864d3bb5d2c3cd3
Ade Indriawan <aindriawan1@gmail.com>
2021-12-12 14:11:50 +0700

    delete .htaccess file and add security meta tag inside head tag html master

--- resources/views/backview/master.blade.php
+++ resources/views/backview/master.blade.php
@@ -16,6 +16,7 @@
 
     <meta name="keywords" content="bootstrap, bootstrap admin template, admin theme, admin dashboard, dashboard template, admin template, responsive" />
     <meta name="author" content="Codedthemes" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="base-url" content="{{ URL::to('/') }}" />
     <!-- Favicon icon -->
--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,15 @@ use App\Http\Controllers\UsersManagementController;
 
 Route::get('/', [AuthController::class, 'signInPage']);
 Route::middleware(['auth'])->group(function() {
-    Route::get('/dashboard', [PageController::class, 'dashboardPage'])->name('dashboard');
-    Route::get('/users/usersTable', [PageController::class, 'usersTablePage'])->name('users');
-    Route::get('/users/usersForm', [PageController::class, 'usersFormPage'])->name('users.form');
-    Route::get('/units/unitsTable', [PageController::class, 'unitsTablePage'])->name('units');
-    Route::get('/units/unitsForm', [PageController::class, 'unitsFormPage'])->name('units.form');
-    Route::post('/units/store', [UsersManagementController::class, 'storeUnit']);
-    Route::get('/traffic', [PageController::class, 'trafficPage'])->name('traffic');
-    Route::get('/report', [PageController::class, 'reportPage'])->name('report');
-    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/dashboard/', [PageController::class, 'dashboardPage'])->name('dashboard');
+    Route::get('/users/usersTable/', [PageController::class, 'usersTablePage'])->name('users');
+    Route::get('/users/usersForm/', [PageController::class, 'usersFormPage'])->name('users.form');
+    Route::get('/units/unitsTable/', [PageController::class, 'unitsTablePage'])->name('units');
+    Route::get('/units/unitsForm/', [PageController::class, 'unitsFormPage'])->name('units.form');
+    Route::post('/units/store/', [UsersManagementController::class, 'storeUnit']);
+    Route::get('/traffic/', [PageController::class, 'trafficPage'])->name('traffic');
+    Route::get('/report/', [PageController::class, 'reportPage'])->name('report');
+    Route::get('/logout/', [AuthController::class, 'logout'])->name('logout');
 });
-Route::get('/login', [AuthController::class, 'signInPage'])->name('login');
-Route::post('/signin', [AuthController::class, 'signIn'])->name('signin');
+Route::get('/login/', [AuthController::class, 'signInPage'])->name('login');
+Route::post('/signin/', [AuthController::class, 'signIn'])->name('signin');
